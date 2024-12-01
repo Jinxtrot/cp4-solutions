29e9d1b6e7c722d48342a47995a690a716c91f10
Jinxtrot <eperales04@gmail.com>
2024-12-01 17:51:57 -0600

    feat: Add thoughts about mastermind

--- ch2/1DArrayManipulation/Harder/Kattis/6-Mastermind.py
+++ ch2/1DArrayManipulation/Harder/Kattis/6-Mastermind.py
@@ -18,3 +18,13 @@ for index, letter in enumerate(code):
     
 
 print(r,s-r)
+
+"""Firstly, i thought about getting the letters that are in the same position
+in both strings. Then, i thought in creating a list of used letters to avoid
+counting the same letter twice. Finally, i thought about getting the minimum
+between the number of times the letter appears in the code and the guess because
+we can't count the same letter twice even if it appears more times in the guess, finally since 
+i was counting each individual letter that was in the guess and code, i subtracted the number of
+right letters from the total number of letters that were in the guess and code to get the number of
+letters that were in the wrong position.
+"""
