8121f7f83fd7979899f4984fca7f06cd25d5e6dd
Jinxtrot <eperales04@gmail.com>
2024-12-04 01:31:38 -0600

    feat: i misread flagquiz AAAAAA

--- ch2/2DArrayManipulation/Harder/Kattis/5-flagquiz.py
+++ ch2/2DArrayManipulation/Harder/Kattis/5-flagquiz.py
@@ -23,10 +23,27 @@ for alternative in alternatives:
         if changes > 0:
             changes_list.append(changes)
             
-    amount_of_changes[alternative] = changes_list
+    amount_of_changes[alternative] = max(changes_list)
 
-print(amount_of_changes)
+minimun = 1000
 
+for alternative in alternatives:
+    if amount_of_changes[alternative]<minimun:
+        minimun = amount_of_changes[alternative]
+
+
+for alternative in alternatives:
+    if minimun==amount_of_changes[alternative]:
+        print(alternative)
+
+"""In this problem, i misread and didnt full understand the problem. I thought that the question of this problem
+was to find the alternative with the least amount of changes respect to ALL the other alternatives.
+In my first approeaches i was trying to sum the amount of changes of each alternative with all the other alternatives
+and the alternative with the least sum of changes would be the answer. But this was wrong, because the question was
+to find the maximun amount of changes of each alternative with all the others, once i had this amount, i had to find the
+minimum of all the maximuns. I was able to solve this problem by creating a dictionary with the amount of changes of each
+alternative with all the others, and then i found the minimum of all the maximuns.
+"""
 
     
 
