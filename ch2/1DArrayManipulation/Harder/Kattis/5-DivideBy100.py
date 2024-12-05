e06321723fc93f00922b96f077d2eb0a769c63a7
Jinxtrot <eperales04@gmail.com>
2024-12-04 22:27:30 -0600

    feat: divideby100 commentary

--- ch2/1DArrayManipulation/Harder/Kattis/5-DivideBy100.py
+++ ch2/1DArrayManipulation/Harder/Kattis/5-DivideBy100.py
@@ -16,3 +16,10 @@ if n_len > m_len:
         print(interger + "." + decimal)
 else:
     print("0." + "0"*((zeroes_on_m)-n_len) + n)
+
+"""
+I was asking why this problem was in the 1D Array Manipulation section, and I realized that the problem is about manipulating the number as a string.
+In the end, m will always be 10**x, where x is a positive integer. So, checking the length of n and m, we can determine which number is the integer and which is the decimal.
+If n has more digits than m, we can split n into an integer and a decimal part. If the decimal part is all zeroes, we can just print the integer part. 
+Otherwise, we print the integer part and the decimal part without the trailing zeroes. If m has more digits than n, we print 0. followed by the difference between the lengths of m and n, and n.
+"""
