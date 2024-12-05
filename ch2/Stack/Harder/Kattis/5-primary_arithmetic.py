9e49c7d12fc5de981a71d41b35bac158d5811a88
Jinxtrot <eperales04@gmail.com>
2024-12-04 23:33:06 -0600

    feat: nvm I thought about the stack implementation and it worked (again)

--- ch2/Stack/Harder/Kattis/5-primary_arithmetic.py
+++ ch2/Stack/Harder/Kattis/5-primary_arithmetic.py
@@ -8,29 +8,34 @@ while True:
     b = b[::-1]
 
     carry = 0
+    count = 0
     result = 0
 
     for index,number in enumerate(a[::-1]):
-        result += int(number) + int(b[index])
+        numbers = []
+        numbers.append(int(number))
+        numbers.append(int(b[index]))
+        numbers.append(carry)
+        result = sum(numbers)
         if result >=10:
-            carry +=1
-            result = 1
+            count +=1
+            carry = 1
         else:
-            result = 0
+            carry = 0
             continue
-    if carry==0:
+    if count==0:
         print("No carry operation.")
-    elif carry==1:
+    elif count==1:
         print("1 carry operation.")
     else:
-        print(f"{carry} carry operations.")
+        print(f"{count} carry operations.")
 
     """
     Well, pretty step forward. First, I thought that the maximun value for a sum of two numbers is 18, so I just need to
     check if the sum is greater than 9, then there is a carry and count from it. But I got wrong the second case because 
     I was not considering the carry in the sum of the numbers. For example 9+0 = 9, but if there is a carry from the previous 
     sum, then 9+0+1 = 10, and the carry now will give you 1 which in my initial approach I was not considering. 
-    Once I fixed that, the code worked fine. I think i didnt use stack in this problem, but I will try to use it in the next
+    Once I fixed that, the code worked fine. Ok, nvm I implemented the code now with stack logic on it.
     """
         
     
